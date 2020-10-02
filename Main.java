2a93080aa94113cf73e7a03b1a2d515ac6b1f4c3
Cheea9337 <cheea9337@wrdsb.ca>
2020-10-02 15:46:44 +0000

    finished q4a3

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * gives the sum of 1 to the user's number
+ * @author Aidan Cheesmond
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // declare the variable
+    int userNum;
+    do{
+      System.out.println("Please enter an integer to count down to");
+      userNum = input.nextInt();
+    }while(userNum <= 0);
+
+    int sum = 0;
     
+ // create a variable to keep track of the number
+    int count = 1;
+    // using a loop to count up to number
+    while(count <= userNum){
+      // add the numbers
+      sum = sum + count;
+      // increase the value of count by one
+      count = count + 1;
+
+    }while(sum <= userNum);
+    System.out.println("The sum from 1 to " + userNum + " is " + sum);
+
+
   }
 }
